8081c63bc826b177b1d96782a856cd074841c87d
almarrim <mosalmarri@gmail.com>
2019-12-05 14:26:57 +0300

    set and connect db

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 
 // Require Route Files
 const indexRouter = require('./app/routes/index');
+//Require the DB Configuration file
+const db = require('./config/db')
+// Establish Database Connection
+mongoose.connect(db, { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('Connected to Mongo')
+})
 
 // Instantiate Express Application Object
 const app = express();
@@ -11,11 +18,13 @@ const app = express();
 // Define PORT for the API to run on
 const port = process.env.PORT || 5000
 
+
+
 /******* Routes ********/
 // Mount imported Routers
 
-// Index
 app.use(indexRouter);
+app.use(articlesRouter);
 
 /******* End of Routes ******/
 // Start the server to listen for request on a given port
